94005b49efeee0bcd82a0a0398b609ac02253ccc
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-03-01 20:39:07 +0100

    Create Testing-Restconf-Connection.py

---
+++ Northbound-Interface/Testing-Restconf-Connection.py
@@ -0,0 +1,10 @@
+#This code is for testing the API integration in the southbound interface
+
+import requests
+
+resp = requests.get('https://todolist.example.com/tasks/')
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+for todo_item in resp.json():
+    print('{} {}'.format(todo_item['id'], todo_item['summary']))
