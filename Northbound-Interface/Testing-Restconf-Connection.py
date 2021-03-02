abfeb9f121cc8a5fdc8c28ed256965adb81f5c80
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-03-02 12:38:52 +0100

    Testing How to solve ApiError

--- Northbound-Interface/Testing-Restconf-Connection.py
+++ Northbound-Interface/Testing-Restconf-Connection.py
@@ -5,6 +5,7 @@ import requests
 resp = requests.get('http://192.168.0.41:8182/restconf/operational/network-topology:network-topology/topology/topology-netconf/node/netopeer/yang-ext:mount/ietf-interfaces:interfaces')
 if resp.status_code != 200:
     # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    #raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    raise prin("Not working")
 for todo_item in resp.json():
     print('{} {}'.format(todo_item['id'], todo_item['summary']))
