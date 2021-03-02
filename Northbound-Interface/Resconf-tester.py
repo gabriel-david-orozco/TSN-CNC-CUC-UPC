6060fc95be19cb405e836fd60bc7dac474edb149
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-03-02 13:17:33 +0100

    Update Resconf-tester.py
    Http instead of https

--- Northbound-Interface/Resconf-tester.py
+++ Northbound-Interface/Resconf-tester.py
@@ -17,7 +17,7 @@ headers = {
 module = "ietf-interfaces:interfaces"
 
 # Remember to modify the node automatically
-url = f"https://{device['ip']}:{device['port']}/restconf/config/network-topology:network-topology/topology/topology-netconf/node/netopeer/yang-ext:mount/{module}"
+url = f"http://{device['ip']}:{device['port']}/restconf/config/network-topology:network-topology/topology/topology-netconf/node/netopeer/yang-ext:mount/{module}"
 
 requests.packages.urllib3.disable_warnings()
 response = requests.get(url, headers=headers, auth=(device['username'], device['password']), verify=False).json()
