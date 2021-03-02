6435ec06db257a6901319c7ef1590c7c93f34123
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-03-02 12:55:28 +0100

    Create Resconf-tester.py
    This is another way of doing the Resconf get function

---
+++ Northbound-Interface/Resconf-tester.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from pprint import pprint
+
+device = {
+   "ip": "192.168.0.41",
+   "username": "admin",
+   "password": "admin",
+   "port": "8182",
+}
+
+headers = {
+      "Accept" : "application/yang-data+json",
+      "Content-Type" : "application/yang-data+json",
+   }
+
+module = "ietf-interfaces:interfaces"
+
+# Remember to modify the node automatically
+url = f"https://{device['ip']}:{device['port']}/restconf/config/network-topology:network-topology/topology/topology-netconf/node/netopeer/yang-ext:mount/{module}"
+
+requests.packages.urllib3.disable_warnings()
+response = requests.get(url, headers=headers, auth=(device['username'], device['password']), verify=False).json()
+
+interfaces = response['ietf-interfaces:interfaces']['interface']
+
+for interface in interfaces:
+   if bool(interface['ietf-ip:ipv4']): //check if IP address is available
+      print(f"{interface['name']} -- {interface['description']} -- {interface['ietf-ip:ipv4']['address'][0]['ip']}")
