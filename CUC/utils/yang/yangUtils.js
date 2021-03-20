5c3b87f21f35baca79fdffb37832e97224291caa
unknown <jordicros997@gmail.com>
2021-03-20 13:08:23 +0100

    Added files to latest commit

---
+++ CUC/utils/yang/yangUtils.js
@@ -0,0 +1,19 @@
+const Yang = require('yang-js');
+const fs = require('fs');
+const path = require('path');
+const talkerJsonSample = require('./json-samples/talker.json');
+let rawTsnSchema = require('./ieee802-dot1q-tsn-types.js').schema;
+Yang.import(path.resolve(__dirname, "./../../../Yang_models/ietf-inet-types.yang"));
+//The following schema is just the interface, instance data should be generated.
+var schema = Yang.parse(rawTsnSchema);
+
+//TODO generate sample data to test the function
+function validateData(instanceData) {
+    try {
+        schema.validate(instanceData);
+        return instanceData;
+    } catch(error) {
+        console.log("Error on validating the instance data")
+        return -1;
+    }
+}
