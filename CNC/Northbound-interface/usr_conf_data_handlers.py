aa2533104e712a04462a635e19e2db03c0116d00
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-04-08 13:33:43 +0200

    Updating the handlers

--- CNC/Northbound-interface/usr_conf_data_handlers.py
+++ CNC/Northbound-interface/usr_conf_data_handlers.py
@@ -5,6 +5,7 @@ from yangson.instance import InstanceRoute
 from jetconf.data import BaseDatastore, DataChange
 from jetconf.helpers import ErrorHelpers, LogHelpers
 from jetconf.handler_base import ConfDataListHandler
+from jetconf.handler_base import ConfDataObjectHandler
 
 JsonNodeT = Union[Dict[str, Any], List]
 epretty = ErrorHelpers.epretty
@@ -14,30 +15,30 @@ debug_confh = LogHelpers.create_module_dbg_logger(__name__)
 # ---------- User-defined handlers follow ----------
 
 
-class JukeboxExampleConfHandler(ConfDataListHandler):
+class JukeboxExampleConfHandler(ConfDataObjectHandler):
     def create_item(self, ii: InstanceRoute, ch: "DataChange"):
         debug_confh(self.__class__.__name__ + " replace triggered")
-        info("Creating item '/ieee802-dot1q-tsn-types-upc-version:group-interface-id' in app configuration")
+        info("Creating item '/ieee802-dot1q-tsn-types-upc-version:tsn-uni' in app configuration")
 
     def create_list(self, ii: InstanceRoute, ch: "DataChange"):
         debug_confh(self.__class__.__name__ + " replace triggered")
-        info("Creating list '/ieee802-dot1q-tsn-types-upc-version:group-interface-id' in app configuration")
+        info("Creating list '/ieee802-dot1q-tsn-types-upc-version:tsn-uni' in app configuration")
 
     def replace_item(self, ii: InstanceRoute, ch: "DataChange"):
         debug_confh(self.__class__.__name__ + " replace triggered")
-        info("Replacing item '/ieee802-dot1q-tsn-types-upc-version:group-interface-id' in app configuration")
+        info("Replacing item '/ieee802-dot1q-tsn-types-upc-version:tsn-uni' in app configuration")
 
     def replace_list(self, ii: InstanceRoute, ch: "DataChange"):
         debug_confh(self.__class__.__name__ + " replace triggered")
-        info("Replacing list '/ieee802-dot1q-tsn-types-upc-version:group-interface-id' in app configuration")
+        info("Replacing list '/ieee802-dot1q-tsn-types-upc-version:tsn-uni' in app configuration")
 
     def delete_item(self, ii: InstanceRoute, ch: "DataChange"):
         debug_confh(self.__class__.__name__ + " delete triggered")
-        info("Deleting item '/ieee802-dot1q-tsn-types-upc-version:group-interface-id' from app configuration")
+        info("Deleting item '/ieee802-dot1q-tsn-types-upc-version:tsn-uni' from app configuration")
 
     def delete_list(self, ii: InstanceRoute, ch: "DataChange"):
         debug_confh(self.__class__.__name__ + " delete triggered")
-        info("Deleting list '/ieee802-dot1q-tsn-types-upc-version:group-interface-id' from app configuration")
+        info("Deleting list '/ieee802-dot1q-tsn-types-upc-version:tsn-uni' from app configuration")
 
 
 def register_conf_handlers(ds: BaseDatastore):
