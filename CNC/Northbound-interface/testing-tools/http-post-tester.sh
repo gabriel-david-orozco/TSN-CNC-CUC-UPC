4eaccecb79d07c5f9bdd4466c2d34e7c845754fc
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-04-14 16:16:23 +0200

    Update http-post-tester.sh

--- CNC/Northbound-interface/testing-tools/http-post-tester.sh
+++ CNC/Northbound-interface/testing-tools/http-post-tester.sh
@@ -2,5 +2,5 @@ CLIENT_CERT="/home/jetconf/data/example-client_curl.pem"
 
 echo "--- POST new artist"
 POST_DATA="@Post-testing.json"
-URL="https://127.0.0.1:8443/restconf/data/ieee802-dot1q-tsn-types-upc-version:tsn-uni/stream-list=12-5A-99-aC-BF-b4:bC-25"
+URL="https://127.0.0.1:8443/restconf/data/ieee802-dot1q-tsn-types-upc-version:tsn-uni"
 curl --http2 -k --cert-type PEM -E $CLIENT_CERT -X POST -d "$POST_DATA" "$URL"
