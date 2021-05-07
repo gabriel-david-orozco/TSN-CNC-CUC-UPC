6a9f9e9acdd7ee40988dc2e45e59e2126472399b
unknown <jordicros997@gmail.com>
2021-05-07 09:28:07 +0200

    Developing auto configuration generation for the endpoints

--- CUC/utils/gate-control-list/gateControlListUtils.js
+++ CUC/utils/gate-control-list/gateControlListUtils.js
@@ -1,14 +1,47 @@
+/**
+ * This module is being strictly tested for 1 TSN flow per endpoint.
+ * More flows need to be tested / enhancanced by the new code tenant.
+ */
+
 const arrayUtils = require('./../arrayUtils');
 
 function computeGCLTalker(list) {
     //From traffic-specification and config response values, a simple GCL can be generated.
-    //1. Get shortest interval
-    var ctr = 0;
-    var interval = 999999999999;
-    while(ctr < list.streamDetails.length) {
-        if(list.streamDetails[ctr].request.interval < interval)
-            interval = list.streamDetails[ctr].request.interval;
-        ctr++;
+    //1 TSN flow escenario. +1 flow scenario is not implemented yet.
+    var interval, vlanTag, timeOffset;
+    if(list.streamDetails.length < 2) {
+        interval = list.streamDetails[0].request.interval;
+        let configs = list.streamDetails[0].config['interface-configuration']['interface-list'][0]['config-list'];
+        for (let i=0; i<configs.length; i++) {
+            switch(Object.keys(configs[i])[1]) {
+                case 'ieee802-vlan-tag':
+                    vlanTag = configs[i]['ieee802-vlan-tag'];
+                    break;
+                case 'time-aware-offset':
+                    timeOffset = configs[i]['time-aware-offset'];
+                    break;
+            }
+        }
+        //With time aware offset, vlan info, traffic specification and interval it is possible to build a GCL for the endpoint.
+
+        //Burst transmit time. 100Mbps assumed.
+
+        //Fit it in the interval with time-aware-offset
+
+        //Set the gate states in order to permit the 'vlanPriorityId' queue to transmit at that specific time interval.
+
+        //If conflict with other priorities, CBS should be handled. (Possible TODO: )
+
+    } else {
+        //1. Get shortest interval
+        var ctr = 0;
+        interval = 9999999;
+        while(ctr < list.streamDetails.length) {
+            if(list.streamDetails[ctr].request.interval < interval)
+                interval = list.streamDetails[ctr].request.interval;
+            ctr++;
+        }
+        //TODO: 
     }
 }
 
