816bff4a27b99cccb3302cb9ee45ad3894243f79
unknown <jordicros997@gmail.com>
2021-05-12 11:55:27 +0200

    Test write variable

--- CUC/opc-ua-client/opcua-client.js
+++ CUC/opc-ua-client/opcua-client.js
@@ -23,7 +23,6 @@ async function connectOpcUaServer(endpointUrl) {
     await client.connect(endpointUrl);
 
     session = await client.createSession(); 
-    const tsnInterface = await session.readVariableValue("ns=1;i=1000")
     const streamId = await session.readVariableValue("ns=1;i=1001")
     const endpointType = await session.readVariableValue("ns=1;i=1002");
     const macAddress = await session.readVariableValue("ns=1;i=1003");
@@ -90,6 +89,7 @@ async function connectOpcUaServer(endpointUrl) {
 }
 async function sendConfigToEndpoints(endpointUrl, config) {
     //TODO
+    await session.writeSingleNode("ns=1;i=1011", config)
 }
 
 module.exports.connectOpcUaServer = connectOpcUaServer;
