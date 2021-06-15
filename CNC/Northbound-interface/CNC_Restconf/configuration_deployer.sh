eec203c440141df448725315cc74a0745dd0f311
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-06-15 21:36:02 +0200

    Update configuration_deployer.sh

--- CNC/Northbound-interface/CNC_Restconf/configuration_deployer.sh
+++ CNC/Northbound-interface/CNC_Restconf/configuration_deployer.sh
@@ -1,6 +1,6 @@
 python3 -m pip uninstall /home/jukebox-jetconf/jetconf_jukebox/jetconf_jukebox
 rm /home/jukebox-jetconf/jetconf_jukebox/*.py
 cp *.py /home/jukebox-jetconf/jetconf_jukebox/
-python3 -m pip install /home/jukebox-jetconf/.
+python3 -m pip install /home/jukebox-jetconf/jetconf_jukebox/.
 jetconf -c /home/jetconf/data/example-config.yaml
 # you just leave it at the moment to start automizing the deploying process
