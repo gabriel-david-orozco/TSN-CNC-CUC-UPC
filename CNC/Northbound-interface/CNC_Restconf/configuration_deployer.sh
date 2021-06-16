a16ce063bb9da42d84a999c8f80b9af27a23003f
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-06-16 10:50:35 +0200

    Automatic update of the yang model

--- CNC/Northbound-interface/CNC_Restconf/configuration_deployer.sh
+++ CNC/Northbound-interface/CNC_Restconf/configuration_deployer.sh
@@ -3,4 +3,7 @@ rm /home/jukebox-jetconf/jetconf_jukebox/*.py
 cp *.py /home/jukebox-jetconf/jetconf_jukebox/
 python3 -m pip install /home/jukebox-jetconf/.
 jetconf -c /home/jetconf/data/example-config.yaml
+
+rm -r /home/jukebox-jetconf/yang-modules/ieee802-dot1q-tsn-types-upc-version@2018-02-15.yang
+cp /home/TSN-CNC-CUC-UPC/Yang_models/ieee802-dot1q-tsn-types-upc-version-v2@2018-02-15.yang /home/jukebox-jetconf/yang-modules/ieee802-dot1q-tsn-types-upc-version@2018-02-15.yang
 # you just leave it at the moment to start automizing the deploying process
--- CNC/Northbound-interface/testing-tools/http-get.sh
+++ CNC/Northbound-interface/testing-tools/http-get.sh
@@ -1,1 +1,1 @@
-curl --http2 -k --cert-type PEM -E /example-client_curl.pem -X GET https://127.0.0.1:8443/restconf/data
+curl --http2 -k --cert-type PEM -E /home/jetconf/data/example-client_curl.pem -X GET https://127.0.0.1:8443/restconf/data
