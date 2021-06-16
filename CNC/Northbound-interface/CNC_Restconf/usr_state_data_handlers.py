1625be504d7eefc879a0a42ddc8cd2f4ec2a3fb0
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-06-16 17:33:15 +0200

    Update usr_state_data_handlers.py

--- CNC/Northbound-interface/CNC_Restconf/usr_state_data_handlers.py
+++ CNC/Northbound-interface/CNC_Restconf/usr_state_data_handlers.py
@@ -22,13 +22,13 @@ class CNC_RestconfStateHandler_configuration_status_info(StateDataContainerHandl
 # This handler will generate /ieee802-dot1q-tsn-types-upc-version:tsn-uni/stream-list/configuration/status-info/talker-status
 class CNC_RestconfStateHandler_configuration_talker_status(StateDataContainerHandler):
     def generate_node(self, node_ii: InstanceRoute, username: str, staging: bool) -> JsonNodeT:
-        return 1 # should be 1
+        return "1" # should be 1
 
 # This handler will generate /ieee802-dot1q-tsn-types-upc-version:tsn-uni/stream-list/configuration/status-info/listener-status
 class CNC_RestconfStateHandler_configuration_listener_status(StateDataContainerHandler):
     def generate_node(self, node_ii: InstanceRoute, username: str, staging: bool) -> JsonNodeT:
 
-        return 2 # should be 1
+        return "2" # should be 1
 
 # This handler will generate /ieee802-dot1q-tsn-types-upc-version:tsn-uni/stream-list/configuration/status-info/failure-code
 class CNC_RestconfStateHandler_configuration_failure_code(StateDataContainerHandler):
