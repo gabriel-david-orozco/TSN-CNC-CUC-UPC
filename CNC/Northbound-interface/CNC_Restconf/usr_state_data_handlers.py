4e0d882ad24f6872a7d197e5cb0f84377cb53cfd
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-06-16 20:00:27 +0200

    Update usr_state_data_handlers.py

--- CNC/Northbound-interface/CNC_Restconf/usr_state_data_handlers.py
+++ CNC/Northbound-interface/CNC_Restconf/usr_state_data_handlers.py
@@ -15,8 +15,8 @@ class CNC_RestconfStateHandler_configuration(StateDataContainerHandler):
 
         payload= {
         "status-info": {
-            "talker_status": "1",
-            "listener_status": "2",
+            "talker_status": 1,
+            "listener_status": 2,
             "failure-code": "0"
             }
         }
