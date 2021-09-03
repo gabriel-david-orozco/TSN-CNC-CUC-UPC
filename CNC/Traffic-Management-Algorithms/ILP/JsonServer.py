b75d27424487aa8bfb2eb9728e1d00aa5c467932
gabriel-david-orozco <gabriel.david.orozco@upc.edu>
2021-09-03 17:03:05 +0200

    Initial Steps to Cassandra Connection

--- CNC/Traffic-Management-Algorithms/ILP/JsonServer.py
+++
@@ -1,5 +0,0 @@
-from cassandra.cluster import Cluster
-
-cluster = Cluster(contact_points=['172.17.0.3'], port=9042)
-session = cluster.connect()
-session.execute("""CREATE KEYSPACE mikeyspace WITH replication = {'class' : 'SimpleStrategy', 'replication_factor':1};""")
