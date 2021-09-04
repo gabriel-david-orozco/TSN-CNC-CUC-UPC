68bbc2d7e35125d3cf32558f1731a945b6ddff6c
gabriel-david-orozco <gabriel.david.orozco@upc.edu>
2021-09-04 12:28:19 +0200

    Updating Database Handler

--- CNC/Northbound-interface/Cassandra Connection.py
+++ CNC/Northbound-interface/Cassandra Connection.py
@@ -5,12 +5,12 @@ session = cluster.connect()
 session.execute("""DROP KEYSPACE newkeyspace""")
 session.execute("""CREATE KEYSPACE newkeyspace WITH replication = {'class' : 'SimpleStrategy', 'replication_factor':1};""")
 session.execute("""USE newkeyspace""")
-session.execute("""DELETE TABLE usuarios""")
+#session.execute("""DROP TABLE usuarios""")
 session.execute("""CREATE TABLE usuarios (
   usrid int PRIMARY KEY,
   nombre text,
   ape1 text
 );""")
-session.execute("""INSERT INTO usuarios (usrid,nombre, ape1) VALUES (1, "Gabriel", "Orozco");""")
+session.execute("""INSERT INTO usuarios (usrid, nombre, ape1) VALUES (1, 'Gabriel', 'Orozco');""")
 
 session.execute("""select * from usuarios;""")
