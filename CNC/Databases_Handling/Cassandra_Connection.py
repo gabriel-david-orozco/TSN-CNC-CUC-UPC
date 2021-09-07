cd2403e8d5ce21ef34fed1a1ed581ec936e8c368
gabriel-david-orozco <gabriel.david.orozco@upc.edu>
2021-09-07 18:36:20 +0200

    Adding methods for creating Cassandra DB

--- CNC/Databases_Handling/Cassandra_Connection.py
+++ CNC/Databases_Handling/Cassandra_Connection.py
@@ -1,23 +1,8 @@
 from cassandra.cluster import Cluster
 
-# cluster = Cluster(contact_points=['172.17.0.3'], port=9042)
-# session = cluster.connect()
-# session.execute("""DROP KEYSPACE newkeyspace""")
-# session.execute("""CREATE KEYSPACE newkeyspace WITH replication = {'class' : 'SimpleStrategy', 'replication_factor':1};""")
-# session.execute("""USE newkeyspace""")
-# #session.execute("""DROP TABLE usuarios""")
-# session.execute("""CREATE TABLE usuarios (
-#   usrid int PRIMARY KEY,
-#   nombre text,
-#   ape1 text
-# );""")
-# session.execute("""INSERT INTO usuarios (usrid, nombre, ape1) VALUES (1, 'Gabriel', 'Orozco');""")
-
-# session.execute("""select * from usuarios;""")
-
 class Cassandra_connecion():
   def __init__(self) :
-      self.cluster = Cluster(contact_points=['172.17.0.2'], port=9042)
+      self.cluster = Cluster(contact_points=['localhost'], port=9042)
       self.session = self.cluster.connect()
       try :
         self.session.execute("""CREATE KEYSPACE cassandra_namespace \
@@ -25,7 +10,8 @@ class Cassandra_connecion():
       except :
         pass 
   
-  
+# This method receives the values in a list, each element is the value, the type and if it is or not Foreign key
+# E.g., columns = ["usrid text PRIMARY KEY"," nombre text", " ape1 text"]
   def table_creator(self, table_name, columns):
     self.session.execute("""USE cassandra_namespace""")
     try:
@@ -34,21 +20,30 @@ class Cassandra_connecion():
       print("The table does not exit, creating ")
     special_characters = "']["
     for element in special_characters:
-      print("entering 1 time", element)
       columns = str(columns).replace(element, "")
     self.session.execute("CREATE TABLE " + table_name + "( " + str(columns) + ");")
 
+# This method receives the values as a dictionary where the key are the names of the columns in the table
+#and the values are the values to give
+# E.g., filling_values = {"usrid": "1", "nombre": "Gabriel", "ape1": "Orozco"}
+
   def insert_in_table(self, table_name, keyed_values):
     special_characters = "]["
-    values = keyed_values
+    values = str(list(keyed_values.values()))
     for element in special_characters:
-      values = str(list(values.values())).replace(element, "")
+      values = values.replace(element, "")
     keys = ", ".join(keyed_values.keys())
-    print(keys, values)
-    self.session.execute("INSERT INTO " + table_name+ " (" + keys + ") VALUES ("+ values +");")
+    self.session.execute("INSERT INTO " + table_name + " (" + keys + ") VALUES ("+ values +");")
+  
+  def find_in_table(self, table_name, element="*"):
+    query_result = self.session.execute("select " + element + " from " + table_name + ";")
+    row = query_result.one()
+    return row
 
-cassandra = Cassandra_connecion()
-columns = ["usrid text PRIMARY KEY"," nombre text", " ape1 text"]
-filling_values = {"usrid": "1", "nombre": "Gabriel", "ape1": "Orozco"}
-cassandra.table_creator("Topology", columns)
-cassandra.insert_in_table("Topology", filling_values)
+
+# cassandra = Cassandra_connecion()
+# columns = ["usrid text PRIMARY KEY"," nombre text", " ape1 text"]
+# filling_values = {"usrid": "1", "nombre": "Gabriel", "ape1": "Orozco"}
+# cassandra.table_creator("Topology", columns)
+# cassandra.insert_in_table("Topology", filling_values)
+# print(cassandra.find_in_table("Topology") )
