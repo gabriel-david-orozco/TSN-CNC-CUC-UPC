13882968ceeab511ea15d13266ebb08d0575a6cc
gabriel-david-orozco <79850124+gabriel-david-orozco@users.noreply.github.com>
2021-10-06 16:25:14 +0200

    Finding mistake

--- CNC/Traffic-Management-Algorithms/ILP/Microservices-Code/Dependencies_scheduler.sh
+++ CNC/Traffic-Management-Algorithms/ILP/Microservices-Code/Dependencies_scheduler.sh
@@ -12,5 +12,13 @@ pip install pandas
 pip install networkx
 pip install matplotlib
 pip install glpk
-wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
-bash /tmp/Anaconda3-2021.05-Linux-x86_64.sh
+cd ~
+mkdir tmp
+wget -P tmp https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
+bash tmp/Anaconda3-2021.05-Linux-x86_64.sh -b
+source .bashrc
+conda install -c conda-forge glpk
+conda install -c conda-forge/label/gcc7 glpk
+conda install -c conda-forge/label/broken glpk
+conda install -c conda-forge/label/cf201901 glpk
+conda install -c conda-forge/label/cf202003 glpk
