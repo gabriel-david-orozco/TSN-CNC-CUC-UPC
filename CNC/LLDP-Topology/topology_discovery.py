d592ffb41ce21f127e04e1c0dab5d9a1cbc28eb6
gabriel-david-orozco <gabriel.david.orozco@upc.edu>
2021-11-19 19:02:50 +0100

    Creating topology discovery

---
+++ CNC/LLDP-Topology/topology_discovery.py
@@ -0,0 +1,22 @@
+import paramiko, time
+import os
+
+try:
+    os.mkdir('devices')
+except:
+    print("already_created")
+
+with open('sw_addresses.conf', 'r') as address_file:
+    addresses=address_file.read().split('\n')
+    print(addresses, type(addresses))
+
+for ip in addresses:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, username='soc-e', password='soc-e')
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('sudo lldpcli show neighbors')
+    time.sleep(1)
+    data = ssh_stdout.read()
+    ssh.close()
+    with open('devices/topology_'+ ip + '.txt', 'w') as f:
+        f.write(str(data))
